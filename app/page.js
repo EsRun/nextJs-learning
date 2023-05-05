8191067d2585b5713cf4e26be416dd2ce72bf7b2
비엔나 <sure@paran.com>
2023-05-05 20:19:28 +0900

    test : useEffect

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("main");
+  });
   return (
     <div>
       <h4>메인 페이지</h4>
