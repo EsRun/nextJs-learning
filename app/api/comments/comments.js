742b89f17499aaff3075b91132b0c28b6a425196
비엔나 <sure@paran.com>
2023-05-07 21:17:46 +0900

    add : api test code

---
+++ app/api/comments/comments.js
@@ -0,0 +1,9 @@
+const comments = [
+  { id: 1, text: "comment 1" },
+  { id: 2, text: "comment 2" },
+  { id: 3, text: "comment 3" },
+];
+
+export default function handler(req, res) {
+  res.status(200).json(comments);
+}
